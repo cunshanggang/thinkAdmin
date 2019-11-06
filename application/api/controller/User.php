799644e26e9c8bafe9f4aa94537e6152fd796619
cunshanggangg <cunshanggangg@mail.com>
2019-11-06 18:21:11 +0800

    Test model function.

--- application/api/controller/User.php
+++ application/api/controller/User.php
@@ -2,22 +2,21 @@
 namespace app\api\controller;
 
 use think\Controller;
-use app\model\SystemUser;
 use think\Model;
-use app\common\logic\SystemUser as STU;
+use app\common\logic\SystemUser;
 class User extends Controller
 {
     public function saveUserData()
     {
 //        $logic = logic("SystemUser");
 //        echo $logic;exit;
-        $logic = new STU();
-        echo $logic->test();
-        exit;
+//        $logic = new STU();
+//        echo $logic->test();
+//        exit;
 //        print_r($this->request->post());
-//        $model = model("SystemUser");
-//        $result = $model->select();
-//        echo "<pre>";
-//        print_r($result);
+        $model = model("SystemUser");
+        $result = $model->select();
+        echo "<pre>";
+        print_r($result);
     }
 }
